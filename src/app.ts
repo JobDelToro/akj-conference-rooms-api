1d54a0dc25fa3e75a67ddbe60f24f63f738a8e54
Job Del Toro <jobdeltoro@Jobs-MacBook-Pro-2.local>
2025-10-01 19:09:15 -0600

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to your TypeScript Express API!');
+});
+
+app.get('/kaoru', (req: Request, res: Response) => {
+  res.send('Kaoru is a good man!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is shining at http://localhost:${port}`);
+});
